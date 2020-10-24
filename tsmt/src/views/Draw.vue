7aa59fc5631644e46db4ac1ea0311e4ec9395842
f0lg0 <folgonileonardo@gmail.com>
2020-10-24 19:33:23 +0200

    new: rendering classes and attributes

--- tsmt/src/views/Draw.vue
+++ tsmt/src/views/Draw.vue
@@ -19,27 +19,50 @@ export default {
     },
     created() {
         EventBus.$on("parsedPayload", (payload) => {
+            console.log(payload);
             const drawable = document.getElementById("classesArea");
             drawable.innerHTML = "";
 
             const classes = Object.keys(payload);
 
             for (const c of classes) {
-                const classDiv = document.createElement("div");
-                classDiv.style.width = "200px";
-                classDiv.style.height = "300px";
-                classDiv.style.border = "2px solid black";
-                classDiv.style.padding = "15px";
-                classDiv.innerText = c;
+                const wrapper = document.createElement("div");
+                let wrapperHeight = 100;
 
-                drawable.appendChild(classDiv);
+                wrapper.style.width = "200px";
+                wrapper.style.minHeight = "100px";
+                wrapper.style.border = "2px solid black";
+                wrapper.style.borderRadius = "5px";
+
+                const classDiv = document.createElement("div");
+                classDiv.style.width = "198px";
+                classDiv.style.height = "28px";
+                classDiv.style.borderBottom = "2px solid black";
+                classDiv.innerText = c;
+                classDiv.style.textAlign = "center";
+
+                const attributes = document.createElement("div");
+                attributes.style.width = "200px";
+                attributes.style.padding = "10px";
+
+                for (let i = 0; i < payload[c].length; i++) {
+                    const temp = document.createElement("div");
+                    temp.innerText =
+                        payload[c][i].attribute + ": " + payload[c][i].type;
+
+                    attributes.appendChild(temp);
+                    wrapper.style.height = `${wrapperHeight}px`;
+                    wrapperHeight += 20;
+                }
+
+                wrapper.appendChild(classDiv);
+                wrapper.appendChild(attributes);
+
+                drawable.appendChild(wrapper);
             }
         });
     },
 
-    afterMount() {
-        this;
-    },
     components: {
         Editor,
     },
@@ -66,5 +89,13 @@ export default {
 
     position: relative;
     top: 70px;
+}
+
+.uml-class {
+    width: 200px;
+    height: 20px;
+    border: 2px solid black;
+    padding: 10px;
+    text-align: center;
 }
 </style>
